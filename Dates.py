81955f3ff8ccf21abaeb03d5f4278f320dbaebc5
Nifemi Ekemode <158369103+Nifemi2011@users.noreply.github.com>
2024-05-23 20:09:02 -0500

    Using dates in pyhton
    This allows you to add typical dates used in the world to ur code!

---
+++ Dates.py
@@ -0,0 +1,28 @@
+# What is Dates in pyhton? ⭐
+#  A date is a typical date like the fruit no; In python terms Dates are are way of importing the global/worldwride used date info into ur code>
+# Examples:
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+# The code above explains and will also show an output like the one shown below:
+#2024-05-23 19:18:42.035094 ⬅️ This will allow us to see the current time in our code (This does not show updated time)👍
+
+# Another Example: 
+ import datetime
+
+x= datetime.datetime.now()
+print(X)
+print("f'The time in your current location now is{x}')
+
+
+   ➡️ Date Objects:
+
+import datetime
+x = datetime.datetime(2024,5,23)
+print(x)
+
+@Nifemi_2011 Vscode
+@Nifemi_2011
